714f47a32b1f8d5c47f4433fde66d8a3eacd4d16
Govind Thakur <govind.thakur11@gmail.com>
2019-08-22 16:05:43 +0530

    Task- word count

---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+This is a similar-to-task 2 project
+the project takes a set of words as input:
+Output:
+-No. of words
+-No of occurences of each word
--- Web_word_count/view.py
+++ Web_word_count/view.py
@@ -1,30 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def home(request):
     return render(request, 'home.html')
 
 def count(request):
     fulltext=request.GET['fulltext']
-
     word_list = fulltext.split()
-    words=[]
+    worddictionary = {}
+
     for word in word_list:
-        if word in words:
-            continue
+        if word in worddictionary:
+            worddictionary[word] += 1
         else:
-            words.append(word)
-    count=[]
+            worddictionary[word] = 1
 
-    for word in words:
-        i=0
-        if word in word_list:
-            for j in range(len(word_list)):
-                if word_list[j]==word:
-                    i+=1
-            count.append(i)
+    sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
 
-    return render(request, 'count.html',{'fulltext': fulltext, 'count': len(word_list),'words': words,'word_count':count,'terms':len(count)})
+    return render(request, 'count.html',{'fulltext':fulltext,'count':len(word_list),'sortedwords':sortedwords})
 
 def about(request):
     return render(request,'about.html')
